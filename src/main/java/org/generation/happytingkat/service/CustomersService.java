5d26590ebb2a77c60e68ee3e4b18491462fe2dd1
jcmonkey87 <fuzhonglong@gmail.com>
2022-06-15 18:30:17 +0800

    added CustomersServiceSQL and CustomerService methods

--- src/main/java/org/generation/happytingkat/service/CustomersService.java
+++ src/main/java/org/generation/happytingkat/service/CustomersService.java
@@ -1,4 +1,15 @@
 package org.generation.happytingkat.service;
 
+import org.generation.happytingkat.repository.entity.Customers;
+
+import java.util.List;
+
 public interface CustomersService {
+
+    List<Customers> all();
+
+    void delete (int customerId);
+
+    Customers findById (int itemId);
+
 }
--- src/main/java/org/generation/happytingkat/service/PackageServiceMySQL.java
+++ src/main/java/org/generation/happytingkat/service/PackageServiceMySQL.java
@@ -1,29 +1,25 @@
 package org.generation.happytingkat.service;
 
+import org.generation.happytingkat.repository.entity.Package;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
-/*@Service
+@Service
 public class PackageServiceMySQL implements PackageService {
 
     public PackageServiceMySQL() {}
-
     @Override
-    public void delete ( int packageId )
-    {
-
-    }
-
-    @Override
-    public List<Package> all()
-    {
+    public List<Package> all() {
         return null;
     }
 
     @Override
-    public Package findById ( int packageId)
-    {
+    public void delete(int packageId) {
 
     }
 
-}*/
+    @Override
+    public Package findById(int packageId) {
+        return null;
+    }
+}
