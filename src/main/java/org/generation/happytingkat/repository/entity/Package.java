84c115c4512f3bbcda985c95224628b0d16bfe24
desporamon <dygchua21@gmail.com>
2022-06-15 15:16:32 +0800

    added package entity

--- src/main/java/org/generation/happytingkat/repository/entity/Package.java
+++ src/main/java/org/generation/happytingkat/repository/entity/Package.java
@@ -1,4 +1,83 @@
 package org.generation.happytingkat.repository.entity;
 
 public class Package {
+
+    private Integer id;
+    private String name;
+    private double price;
+    private String type;
+    private String imageUrl;
+    private String cuisine;
+    private String description;
+
+    public Package() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getCuisine() {
+        return cuisine;
+    }
+
+    public void setCuisine(String cuisine) {
+        this.cuisine = cuisine;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Package{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", type='" + type + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", cuisine='" + cuisine + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
