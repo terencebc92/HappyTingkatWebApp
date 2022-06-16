29c1129c9fc23857da9611f5af3fcc788f0a11b2
jcmonkey87 <fuzhonglong@gmail.com>
2022-06-16 15:59:49 +0800

    first commit PackageController

--- src/main/java/org/generation/happytingkat/controller/PackageController.java
+++ src/main/java/org/generation/happytingkat/controller/PackageController.java
@@ -1,8 +1,32 @@
 package org.generation.happytingkat.controller;
 
+import org.generation.happytingkat.repository.entity.Package;
+import org.generation.happytingkat.service.PackageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/package")
 public class PackageController {
 
+    final PackageService packageService;
 
+    public PackageController(@Autowired PackageService packageService)
+    {
+        this.packageService = packageService;
+    }
+
+    @CrossOrigin
+    @GetMapping("/all")
+    public List<Package> getPackages()
+    {
+        return packageService.all();
+    }
 
 }
 
