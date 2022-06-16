a68ce228b0df56cdaa900ca908d1da4a32403e1e
jcmonkey87 <fuzhonglong@gmail.com>
2022-06-16 14:44:04 +0800

    Updated PackageServiceMySQL

--- src/main/java/org/generation/happytingkat/service/PackageServiceMySQL.java
+++ src/main/java/org/generation/happytingkat/service/PackageServiceMySQL.java
@@ -1,25 +1,36 @@
 package org.generation.happytingkat.service;
 
+import org.generation.happytingkat.repository.PackageRepository;
 import org.generation.happytingkat.repository.entity.Package;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.util.List;
+
+import java.util.*;
 
 @Service
 public class PackageServiceMySQL implements PackageService {
 
-    public PackageServiceMySQL() {}
+    private final PackageRepository packageRepository;
+    public PackageServiceMySQL(@Autowired PackageRepository packageRepository) {
+        this.packageRepository = packageRepository;
+    }
+
     @Override
-    public List<Package> all() {
-        return null;
+    public List<Package> all()
+    {
+        List<Package> result = new ArrayList<>();
+        packageRepository.findAll().forEach( result :: add );
+        return result;
     }
 
     @Override
     public void delete(int packageId) {
-
+        packageRepository.deleteById(packageId);
     }
 
     @Override
     public Package findById(int packageId) {
         return null;
     }
+
 }
