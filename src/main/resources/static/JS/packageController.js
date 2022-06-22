db3745c83b08ed40083b136f575160ed61a6b38b
terencebc92 <terencebc92@gmail.com>
2022-06-22 23:33:55 +0800

    Add filter for admin page

--- src/main/resources/static/JS/packageController.js
+++ src/main/resources/static/JS/packageController.js
@@ -7,8 +7,8 @@ class PackagesController {
         this.domainURL_Prod = "https://happytingkat.herokuapp.com/";
 
         // Change this to _Prod/_Dev depending on what you're using
-//        this.allPackageAPI = this.domainURL_Dev + "package/all"
-        this.allPackageAPI = this.domainURL_Prod + "package/all"
+        this.allPackageAPI = this.domainURL_Dev + "package/all"
+//        this.allPackageAPI = this.domainURL_Prod + "package/all"
 
         this.allPackageItems = [];
     }
